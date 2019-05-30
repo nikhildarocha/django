2b0629a3b55b316d94bcde725055393e7ed61679
transmigrator <niks77883@gmail.com>
2019-05-30 15:05:45 +0530

    part 1

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ndarocha/Desktop/code/python_projects/django/djangoenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
